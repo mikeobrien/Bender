4aa475359db22a3335dfd588aa518c7544eb24c4
mob <mob@mikeobrien.net>
2014-02-08 00:54:50 -0500

    Migrated to grunt.

---
+++ gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-msbuild');
+    grunt.loadNpmTasks('grunt-dotnet-assembly-info');
+    grunt.loadNpmTasks('grunt-nunit-runner');
+    grunt.loadNpmTasks('grunt-nuget');
+
+    grunt.registerTask('default', ['msbuild', 'nunit']);
+    grunt.registerTask('ci', ['assemblyinfo', 'msbuild', 'nunit', 'nugetpack']);
+    grunt.registerTask('deploy', ['assemblyinfo', 'msbuild', 'nunit', 'nugetpack', 'nugetpush']);
+
+    grunt.initConfig({
+        assemblyinfo: {
+            options: {
+                files: ['src/Bender.sln'],
+                info: {
+                    version: process.env.BUILD_NUMBER, 
+                    fileVersion: process.env.BUILD_NUMBER
+                }
+            }
+        },
+        msbuild: {
+            src: ['src/Bender.sln'],
+            options: {
+                projectConfiguration: 'Release',
+                targets: ['Clean', 'Rebuild'],
+                stdout: true
+            }
+        },
+        nunit: {
+            options: {
+                files: ['src/Bender.sln'],
+                teamcity: true
+            }
+        },
+        nugetpack: {
+            flexo: {
+                src: 'Bender.nuspec',
+                dest: './'
+            },
+            options: {
+                version: process.env.BUILD_NUMBER
+            }
+        },
+        nugetpush: {
+            flexo: {
+                src: '*.nupkg'
+            },
+            options: {
+                apiKey: process.env.NUGET_API_KEY
+            }
+        }
+    });
+}
